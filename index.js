5a8a0ad383421ae64a31f7f82f41f8e7301d1ccb
Tarun <tarunpahade6969@gmail.com>
2023-09-29 18:18:34 +0530

    Added Otp twilio

--- index.js
+++ index.js
@@ -1,79 +1,99 @@
-const express= require('express');
-const bodyParser=require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-
+const sid = "ACbf2608b126a238d429463d915859023d";
+const auth_token = "4335230463b820cc6d7b6fcbf50237fd";
+var twilio = require("twilio")(sid, auth_token);
 
 //Notification xV790J89vD46plywun_cf4-vpi8ghi7Q04gv-QuQ
-const transactionRouter = require('./src/router/transactionRouter');
-const taskRouter = require('./src/router/tasksRouter');
-const studentRouter = require('./src/router/studentFund');
-const parentRouter = require('./src/router/parentFund');
-const loginRouter = require('./src/router/login');
-const notificationsRouter = require('./src/router/notification');
+const transactionRouter = require("./src/router/transactionRouter");
+const taskRouter = require("./src/router/tasksRouter");
+const studentRouter = require("./src/router/studentFund");
+const parentRouter = require("./src/router/parentFund");
+const loginRouter = require("./src/router/login");
+const notificationsRouter = require("./src/router/notification");
 
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-require('dotenv').config()
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+require("dotenv").config();
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(express.json())
-app.use(bodyParser.urlencoded({
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(express.json());
+app.use(
+  bodyParser.urlencoded({
     extended: true,
-    limit: '50mb',
-  }));
-  
+    limit: "50mb",
+  })
+);
 
-
-io.on('connection', (socket) => {
- 
-  socket.on('newTask', (orderData) => {
-    io.emit('newTask', orderData);
-  });
-  
-  socket.on('TaskSentForApproval', (orderData) => {
-    console.log('theb task sent for approval',orderData);
-    io.emit('TaskSentForApproval', orderData);
-  });
-  socket.on('ApproveTask', (orderData) => {
-    io.emit('ApproveTask', orderData);
-  });
-  socket.on('RemindTask', (orderData) => {
-    io.emit('RemindTask', orderData);
-  });
-  
-  socket.on('New Child', (orderData) => {
-    io.emit('New Child', orderData);
+io.on("connection", (socket) => {
+  socket.on("newTask", (orderData) => {
+    io.emit("newTask", orderData);
   });
 
-  socket.on('RequestedForMoney', (orderData) => {
-   console.log(orderData);
-    io.emit('RequestedForMoney', orderData);
+  socket.on("TaskSentForApproval", (orderData) => {
+    console.log("theb task sent for approval", orderData);
+    io.emit("TaskSentForApproval", orderData);
+  });
+  socket.on("ApproveTask", (orderData) => {
+    io.emit("ApproveTask", orderData);
+  });
+  socket.on("RemindTask", (orderData) => {
+    io.emit("RemindTask", orderData);
   });
 
-  
+  socket.on("New Child", (orderData) => {
+    io.emit("New Child", orderData);
+  });
+
+  socket.on("RequestedForMoney", (orderData) => {
+    console.log(orderData);
+    io.emit("RequestedForMoney", orderData);
+  });
 });
 
-  const PORT=5000 || process.env.PORT
+const PORT = 5000 || process.env.PORT;
 
+const IP_ADDRESS = "192.168.106.253";
+app.use("/transactions", transactionRouter);
+app.use("/tasks", taskRouter);
+app.use("/studentFund", studentRouter);
+app.use("/parentFund", parentRouter);
+app.use("/login", loginRouter);
+app.use("/notification", notificationsRouter);
 
-  const IP_ADDRESS = '192.168.106.253'
-app.use('/transactions', transactionRouter);
-app.use('/tasks', taskRouter);
-app.use('/studentFund', studentRouter);
-app.use('/parentFund', parentRouter);
-app.use('/login', loginRouter);
-app.use('/notification', notificationsRouter);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+app.post("/sendOtp", async (req, res) => {
+  console.log("hiii");
+  const {otp} = req.body;
+  try {
+    const message = twilio.messages
+      .create({
+        from: "+12565738101",
+        to: "+918010669013",
+        body: `this is testing otp is ${otp}`,
+      })
+      .then(function (res) {
+        console.log("message has sent!");
+      })
+      .catch(function (err) {
+        console.log(err.message);
+      });
+    res.send({ message });
+  } catch (error) {
+    console.log(error.message);
+    res.send({ error: error.message });
+  }
+});
 
-app.get('/hii', (req, res) => {
-    res.send('Hello World');
-})
+app.get("/hii", (req, res) => {
+  res.send("Hello World");
+});
 //new port
 
-
 server.listen(PORT, () => {
-    console.log('Server is running on port 3000');
-})
+  console.log("Server is running on port 3000");
+});
